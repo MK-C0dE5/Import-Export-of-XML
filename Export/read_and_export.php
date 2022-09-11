00625f4713cafb2da6c19586b76f4d2f402961bf
Mayur Khadde <102829101+MK-C0dE5@users.noreply.github.com>
2022-09-11 23:32:50 +0530

    Add

--- Export/read_and_export.php
+++ Export/read_and_export.php
@@ -4,33 +4,26 @@ $query = "SELECT * FROM data1";
 $result = mysqli_query($conn, $query);
 $items = array();
 
-//Store table records into an array
 while( $row = $result->fetch_assoc() ) {
-$items[] = $row;
+    $items[] = $row;
 }
-//Check the export button is pressed or not
 if(isset($_POST["export"])) {
-//Define the filename with current date
-$fileName = "itemdata-".date('d-m-Y').".xls";
+    $fileName = "itemdata-".date('d-m-Y').".xls";
 
-//Set header information to export data in excel format
-header('Content-Type: application/vnd.ms-excel');
-header('Content-Disposition: attachment; filename='.$fileName);
+    header('Content-Type: application/vnd.ms-excel');
+    header('Content-Disposition: attachment; filename='.$fileName);
 
-//Set variable to false for heading
-$heading = false;
+    $heading = false;
 
-//Add the MySQL table data to excel file
-if(!empty($items)) {
-foreach($items as $item) {
-if(!$heading) {
-echo implode("\t", array_keys($item)) . "\n";
-$heading = true;
+    if(!empty($items)) {
+        foreach($items as $item) {
+            if(!$heading) {
+                echo implode("\t", array_keys($item)) . "\n";
+                $heading = true;
+            }
+            echo implode("\t", array_values($item)) . "\n";
+        }
+    }
+    exit();
 }
-echo implode("\t", array_values($item)) . "\n";
-}
-}
-exit();
-}
-
 ?>
